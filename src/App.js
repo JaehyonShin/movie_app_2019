1001b13f482ee568ce50cbf5393b63b45bf4e1d2
JaehyonShin <bacasa2010@naver.com>
2019-10-14 17:49:32 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav,some}){
+  return <h1>I like {fav} {some}</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <Food fav="kimchi" some="123" />
+      <Food   some="123" />
+      <Food fav="sangiopsal"   />
+      <Food fav="chukumi"  some="123" />
     </div>
+    
   );
 }
 
