6cce3362ebd0c220c18eec517a054467f675e5b0
JaehyonShin <bacasa2010@naver.com>
2019-10-15 14:51:31 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import React from 'react';
 
-function Food({fav,some}){
-  return <h1>I like {fav} {some}</h1>
+function Food({fav,some,picture}){
+  return <div>
+    <h1>I like {fav} {some}</h1>
+    <img src = {picture} ></img>
+  </div>
+  
+  
+  
 }
 
+const foodILike = [
+      {
+        name : "kimchi",
+        Image : "https://www.thespruceeats.com/thmb/MYAfoHm73dYZOmwxFdVzZoXCwvU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/kimchi-dish-served-on-a-white-plate-173021127-588b8a945f9b5874ee55f535.jpg"
+      },
+      {
+        name : "kimchi2",
+        Image : "https://www.thespruceeats.com/thmb/MYAfoHm73dYZOmwxFdVzZoXCwvU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/kimchi-dish-served-on-a-white-plate-173021127-588b8a945f9b5874ee55f535.jpg"
+      },
+      {
+        name : "kimchi3",
+        Image : "https://www.thespruceeats.com/thmb/MYAfoHm73dYZOmwxFdVzZoXCwvU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/kimchi-dish-served-on-a-white-plate-173021127-588b8a945f9b5874ee55f535.jpg"
+      }
+    
+    ]
+
+//하나씩 렌더링 하는 방법
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//       <Food fav="kimchi" some="123" />
+//       <Food   some="123" />
+//       <Food fav="sangiopsal"   />
+//       <Food fav="chukumi"  some="123" />
+//     </div>
+    
+//   );
+// }
+
+
+// 여러개 렌더링 방법
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <Food fav="kimchi" some="123" />
-      <Food   some="123" />
-      <Food fav="sangiopsal"   />
-      <Food fav="chukumi"  some="123" />
+      {foodILike.map(dish => <Food fav={dish.name} picture={dish.Image}></Food>)}
     </div>
     
   );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
+
 ReactDOM.render(<App />, document.getElementById('root')); // <App /> 과 같은 것을 component 라고 한다. javascript 와 HTML 사이의 조합 jsx
