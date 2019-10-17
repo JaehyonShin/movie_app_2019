71f73db018dac7a77f02f814804add6f7052793b
JaehyonShin <bacasa2010@naver.com>
2019-10-17 17:39:30 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,45 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types'; //  prop-types 설치 : npm i prop-types => 내가 전달 받은 props가 내가 원하는 props인지를 확인  #2.4 Protection with PropTypes
 
 class App extends React.Component{ //클래스 컴포넌트
-
-    constructor(props){ //리액트가 아닌 자바스크립트에서 생성, 가장 처음 작동
-      super(props);
-      console.log("hello");
-    }
-
     state = {
-      count : 0
+      isLoading : true,
+      movie : []
     };
 
-    add = () => {
-      this.setState(current => ({ count: current.count +1 }))  //자동으로 render를 다시 실행 (변경되는 부분만)
-    };
-    minus = () => {
-      this.setState(current => ({ count: current.count -1 })) //자동으로 render를 다시 실행 (변경되는 부분만)
-    };
-
-    componentDidMount(){  //첫 render 후 작동
-      console.log("component rendered");
+    componentDidMount(){
+      setTimeout(()=>{
+        this.setState({isLoading : false})
+      },6000);
     }
-
-    componentDidUpdate(){ //state 변경 후 render 후 작동
-      console.log("component Updated");
+    render() {
+      const { isLoading } = this.state;
+      return <div>{ isLoading ? "Loading..." : "We are ready"}</div>;
     }
-
-    componentWillUnmount(){ //컴포넌트 소멸시 작동
-      console.log("goodbye, Unmount");
-    }
-
-    render() { //리액트는 자동적으로 모든 class component의 render 메소드를 실행하고자 함
-      console.log("I'm rendering");
-      return (
-             <div> 
-                  <h1>The number is :  {this.state.count}</h1>
-                  <button onClick={this.add}>Add</button>
-                  <button onClick={this.minus}>Minus</button>
-             </div>
-      );
-    } 
 }
 
 export default App;
